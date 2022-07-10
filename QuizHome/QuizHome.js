2f7f7ec73ef907dd4771e6cb1c40a835b5110b2a
AntonioKal <antoniskal85@gmail.com>
2022-07-10 20:47:44 +0300

    js updated

--- QuizHome/QuizHome.js
+++ QuizHome/QuizHome.js
@@ -2,9 +2,9 @@ function showLink1() {
 }
 
 function showLink2() {
-    window.location.href = "https://github.com/AntonioKal/Quiz/blob/main/Quiz%20-%2090's/Quiz90s.html";
+    window.location.href = "https://htmlpreview.github.io/?https://github.com/AntonioKal/Quiz/blob/main/Quiz%20-%2090%27s/Quiz90s.html";
 }
 
 function showLink3() {
-    window.location.href = "https://github.com/AntonioKal/Quiz/blob/main/Quiz%20-%20Corfu/Quizcorfu.html";
+    window.location.href = "https://htmlpreview.github.io/?https://github.com/AntonioKal/Quiz/blob/main/Quiz%20-%20Corfu/Quizcorfu.html";
 }
