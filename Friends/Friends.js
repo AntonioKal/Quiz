bb09fb3092a0fc423bcaaa4c5aee950287116801
AntonioKal <antoniskal85@gmail.com>
2023-11-13 20:23:42 +0200

    Friends added

---
+++ Friends/Friends.js
@@ -0,0 +1,306 @@
+﻿function correct1() {
+    myBtn1.style.opacity = '0.2'
+    myBtn2.style.opacity = '0.2'
+    myBtn3.style.opacity = '1'
+    myBtn3.style.backgroundColor = 'orange'
+    myBtn4.style.opacity = '0.2'
+    risultato1.textContent = "Μπράβο κορίτσια!"
+    tryAgain1.style.display = "none"
+    secret.style.display = 'block'
+    let mynumero = document.getElementById('numero')
+    mynumero.textContent = '6'
+    audio.style.display ="block"
+}
+
+function falso1() {
+    myBtn1.style.opacity = '0.2'
+    myBtn2.style.opacity = '0.2'
+    myBtn3.style.opacity = '0.2'
+    myBtn4.style.opacity = '0.2'
+    myBtn1.style.backgroundColor = ''
+    tryAgain1.style.display = "block"
+    risultato1.textContent = ' '
+}
+
+function reset() {
+    myBtn1.style.opacity = ''
+    myBtn2.style.opacity = ''
+    myBtn3.style.opacity = ''
+    myBtn4.style.opacity = ''
+    myBtn5.style.opacity = ''
+    myBtn6.style.opacity = ''
+    myBtn7.style.opacity = ''
+    myBtn8.style.opacity = ''
+    myBtn9.style.opacity = ''
+    myBtn10.style.opacity = ''
+    myBtn11.style.opacity = ''
+    myBtn12.style.opacity = ''
+    myBtn13.style.opacity = ''
+    myBtn14.style.opacity = ''
+    myBtn15.style.opacity = ''
+    myBtn16.style.opacity = ''
+    myBtn17.style.opacity = ''
+    myBtn18.style.opacity = ''
+    myBtn19.style.opacity = ''
+    myBtn20.style.opacity = ''
+    myBtn21.style.opacity = ''
+    myBtn22.style.opacity = ''
+    myBtn23.style.opacity = ''
+    myBtn24.style.opacity = ''
+    myBtn25.style.opacity = ''
+    myBtn26.style.opacity = ''
+    myBtn27.style.opacity = ''
+    myBtn28.style.opacity = ''
+    myBtn29.style.opacity = ''
+    myBtn30.style.opacity = ''
+    myBtn31.style.opacity = ''
+    myBtn32.style.opacity = ''
+    myBtn33.style.opacity = ''
+    myBtn34.style.opacity = ''
+    myBtn35.style.opacity = ''
+    myBtn36.style.opacity = ''
+    myBtn37.style.opacity = ''
+    myBtn38.style.opacity = ''
+    myBtn39.style.opacity = ''
+    myBtn40.style.opacity = ''
+    myBtn41.style.opacity = ''
+    myBtn42.style.opacity = ''
+    myBtn43.style.opacity = ''
+    tryAgain1.style.display = "none"
+    tryAgain2.style.display = "none"
+    tryAgain3.style.display = "none"
+    tryAgain4.style.display = "none" 
+    tryAgain5.style.display = "none"
+    tryAgain6.style.display = "none"
+    tryAgain7.style.display = "none" 
+    tryAgain8.style.display = "none"
+    tryAgain9.style.display = "none"
+    tryAgain10.style.display = "none"
+    tryAgain11.style.display = "none"
+    tryAgain12.style.display = "none"
+    tryAgain13.style.display = "none"
+    tryAgain14.style.display = "none"
+    risultato.textContent = ' ' 
+    }
+
+function correct2() {
+    myBtn5.style.opacity = '0.2'
+    myBtn6.style.backgroundColor = 'orange'
+    secret.style.display = 'block'
+    numero.textContent = '69'
+    risultato2.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso2() {
+    myBtn5.style.opacity = '0.2'
+    myBtn6.style.opacity = '0.2'
+    tryAgain2.style.display = "block"
+    risultato2.textContent = ' '
+}
+
+function correct3() {
+    myBtn7.style.opacity = '0.2'
+    myBtn8.style.backgroundColor = 'orange'
+    secret.style.display = 'block'
+    numero.textContent = '693'
+    risultato3.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso3() {
+    myBtn7.style.opacity = '0.2'
+    myBtn8.style.opacity = '0.2'
+    tryAgain3.style.display = "block"
+    risultato3.textContent = ' '
+}
+
+
+function correct4() {
+    myBtn9.style.opacity = '0.2'
+    myBtn10.style.backgroundColor = 'orange'
+    secret.style.display = 'block'
+    numero.textContent = '6937'
+    risultato4.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso4() {
+    myBtn9.style.opacity = '0.2'
+    myBtn10.style.opacity = '0.2'
+    tryAgain4.style.display = "block"
+    risultato4.textContent = ' '
+}
+
+
+function correct5() {
+    myBtn11.style.backgroundColor = 'orange'
+    myBtn12.style.opacity = '0.2'
+    myBtn13.style.opacity = '0.2'
+    secret.style.display = 'block'
+    numero.textContent = '69374'
+    risultato5.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso5() {
+    myBtn11.style.opacity = '0.2'
+    myBtn12.style.opacity = '0.2'
+    myBtn13.style.opacity = '0.2'
+    tryAgain5.style.display = "block"
+    risultato5.textContent = ' '
+}
+
+function correct6() {
+    myBtn14.style.backgroundColor = 'orange'
+    myBtn15.style.opacity = '0.2'
+    secret.style.display = 'block'
+    numero.textContent = '693748'
+    risultato6.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso6() {
+    myBtn14.style.opacity = '0.2'
+    myBtn15.style.opacity = '0.2'
+    tryAgain6.style.display = "block"
+    risultato6.textContent = ' '
+}
+
+function correct7() {
+    myBtn16.style.opacity = '0.2'
+    myBtn17.style.opacity = '0.2'
+    myBtn18.style.backgroundColor = 'orange'
+    myBtn19.style.opacity = '0.2'
+    secret.style.display = 'block'
+    numero.textContent = '6937484'
+    risultato7.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso7() {
+    myBtn16.style.opacity = '0.2'
+    myBtn17.style.opacity = '0.2'
+    myBtn18.style.opacity = '0.2'
+    myBtn19.style.opacity = '0.2'
+    tryAgain7.style.display = "block"
+    risultato7.textContent = ' '
+}
+
+function correct8() {
+    myBtn20.style.backgroundColor = 'orange'
+    myBtn21.style.opacity = '0.2'
+    myBtn22.style.opacity = '0.2'
+    myBtn23.style.opacity = '0.2'
+    secret.style.display = 'block'
+    numero.textContent = '69374848'
+    risultato8.textContent = 'Μπράβο κορίτσια!'
+}
+
+
+function falso8() {
+    myBtn20.style.opacity = '0.2'
+    myBtn21.style.opacity = '0.2'
+    myBtn22.style.opacity = '0.2'
+    myBtn23.style.opacity = '0.2'
+    tryAgain8.style.display = "block"
+    risultato8.textContent = ' '
+}
+
+function correct9() {
+    myBtn24.style.backgroundColor = 'orange'
+    myBtn25.style.opacity = '0.2'
+    secret.style.display = 'block'
+    numero.textContent = '693748480'
+    risultato9.textContent = 'Πλησιάζετε...'
+}
+
+
+function falso9() {
+    myBtn24.style.opacity = '0.2'
+    myBtn25.style.opacity = '0.2'
+    tryAgain9.style.display = "block"
+    risultato9.textContent = ' '
+}
+
+function correct10() {
+    myBtn26.style.opacity = '0.2'
+    myBtn27.style.backgroundColor = 'orange'
+    myBtn28.style.opacity = '0.2'
+    secret.style.display = 'block'
+    numero.textContent = '6937484801'
+    risultato10.textContent = 'Κοιτάξτε στην αρχή της σελίδας'
+}
+
+function falso10() {
+    myBtn26.style.opacity = '0.2'
+    myBtn27.style.opacity = '0.2'
+    myBtn28.style.opacity = '0.2'
+    tryAgain10.style.display = "block"
+    risultato10.textContent = ' '
+}
+
+function correct11() {
+    myBtn29.style.opacity = '0.2'
+    myBtn30.style.opacity = '0.2'
+    myBtn31.style.backgroundColor = 'orange'
+    myBtn32.style.opacity = '0.2'
+    secret.style.display = 'block'
+    risultato11.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso11() {
+    myBtn29.style.opacity = '0.2'
+    myBtn30.style.opacity = '0.2'
+    myBtn31.style.opacity = '0.2'
+    myBtn32.style.opacity = '0.2'
+    tryAgain11.style.display = "block"
+    risultato11.textContent = ' '
+}
+
+function correct12() {
+    myBtn33.style.opacity = '0.2'
+    myBtn34.style.opacity = '0.2'
+    myBtn35.style.backgroundColor = 'orange'
+    myBtn36.style.opacity = '0.2'
+    secret.style.display = 'block'
+    risultato12.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso12() {
+    myBtn33.style.opacity = '0.2'
+    myBtn34.style.opacity = '0.2'
+    myBtn35.style.opacity = '0.2'
+    myBtn36.style.opacity = '0.2'
+    tryAgain12.style.display = "block"
+    risultato12.textContent = ' '
+}
+
+
+function correct13() {
+    myBtn37.style.opacity = '0.2'
+    myBtn38.style.opacity = '0.2'
+    myBtn39.style.backgroundColor = 'orange'
+    myBtn40.style.opacity = '0.2'
+    secret.style.display = 'block'
+    risultato13.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso13() {
+    myBtn37.style.opacity = '0.2'
+    myBtn38.style.opacity = '0.2'
+    myBtn39.style.opacity = '0.2'
+    myBtn40.style.opacity = '0.2'
+    tryAgain13.style.display = "block"
+    risultato13.textContent = ' '
+}
+
+function correct14() {
+    myBtn41.style.opacity = '0.2'
+    myBtn42.style.backgroundColor = 'orange'
+    myBtn43.style.opacity = '0.2'
+    secret.style.display = 'block'
+    risultato14.textContent = 'Μπράβο κορίτσια!'
+}
+
+function falso14() {
+    myBtn41.style.opacity = '0.2'
+    myBtn42.style.opacity = '0.2'
+    myBtn43.style.opacity = '0.2'
+    tryAgain14.style.display = "block"
+    risultato14.textContent = ' '
+}
